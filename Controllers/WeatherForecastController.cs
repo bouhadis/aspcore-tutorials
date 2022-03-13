117db3a7e9a260e2db75e863e3b8fe4853b011b2
Soufiane <sunlight722@yahoo.fr>
2022-03-13 14:50:14 +0100

    Added Get with param: itemsCount

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace HealthCheck.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int itemCount)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).Take(itemCount)
+            .ToArray();
+        }
     }
 }
